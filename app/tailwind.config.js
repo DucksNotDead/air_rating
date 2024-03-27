a12fa7b8803c4bc98e1fd18541f9b29b49e2c1cc
aleksandrholuenko <zerogormy@mail.ru>
2024-03-27 22:25:57 +0500

    update

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,7 +3,37 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  
   theme: {
+    colors: {
+      white: '#FFF',
+      black: '#1E1E1E',
+      darkGrey: '#6B6B6B',
+      lightGrey: '#C2C2C2',
+      primary: '#01A4FF',
+      secondary: '#7FD0FD',
+      transparent: '#FFFFFF00'
+    },
+
+    spacing: {
+      0: '0px',
+      xs: '4px',
+      sm: '8px',
+      md: '12px',
+      lg: '24px',
+      xl: '32px',
+    },
+
+    borderRadius: {
+      DEFAULT: '12px',
+      md: '24px',
+      full: '999px'
+    },
+
+    fontFamily: {
+      sans: ['AppFont', 'sans-serif']
+    },
+
     extend: {},
   },
   plugins: [],
