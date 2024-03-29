82784fd3a85b866fede64bb286ff02be268412ea
HealKnix <www.daniiland@gmail.com>
2024-03-29 13:23:29 +0500

    fix

--- app/src/constants/types.ts
+++ app/src/constants/types.ts
@@ -2,6 +2,7 @@ export type AppCompanyType = {
   id: number;
   name: string;
   full_name: string;
+  avg_rating: string;
   avg_arrival_delay_minutes: string;
   avg_delay_departure: string;
   ent_flights: number;
